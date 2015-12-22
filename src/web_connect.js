e5a3a257215cafbcbc2ad2fbbb91f02f7639a9e9
atkaiser <alextkaiser@gmail.com>
2015-12-21 21:37:59 -0500

    Added current temp

--- src/web_connect.js
+++ src/web_connect.js
@@ -1,8 +1,11 @@
+// Global vars
+var forecastUrl = 'https://api.forecast.io/forecast/dee1b0c1a1c5af5520fa94cbd4d99665/';
+
 // Helper function for sending an xml http request
-function xhrRequest(url, type, callback) {
+function xhrRequest(url, type, state, callback) {
   var xhr = new XMLHttpRequest();
   xhr.onload = function () {
-    callback(this.responseText);
+    callback(this.responseText, state);
   };
   xhr.open(type, url);
   xhr.send();
@@ -34,21 +37,104 @@ function convertEastToWest(start) {
   return newTime;
 }
 
+// Send the weather results to the pebble
+function sendWeather(temperature, conditions, city) {
+  // Assemble dictionary using our keys
+  var dictionary = {
+    'KEY_TEMPERATURE': temperature,
+    'KEY_CONDITIONS': conditions,
+    'KEY_CITY': city,
+  };
+
+  Pebble.sendAppMessage(dictionary,
+                        function(e) {
+                          console.log('Weather info sent to Pebble successfully!');
+                        },
+                        function(e) {
+                          console.log('Error sending weather info to Pebble!');
+                        }
+                       );
+}
+
 function getWeatherLocation(pos) {
-  var date = new Date();
-  var dayOfWeek = date.getDay();
+//   var date = new Date();
+//   var dayOfWeek = date.getDay();
   var position = '';
-  if ((dayOfWeek >= 1 && dayOfWeek <= 4) ||
-      (dayOfWeek === 0 && date.getHours() >= 15) ||
-      (dayOfWeek === 5 && date.getHours() < 15)) {
-    // Get San Mateo weather
-    position = '37.561,-122.286';
-  } else {
+//   if ((dayOfWeek >= 1 && dayOfWeek <= 4) ||
+//       (dayOfWeek === 0 && date.getHours() >= 15) ||
+//       (dayOfWeek === 5 && date.getHours() < 15)) {
+//     // Get San Mateo weather
+//     position = '37.561,-122.286';
+//   } else {
     console.log("Lat: " + pos.coords.latitude);
     console.log("Lon: " + pos.coords.longitude);
     position = pos.coords.latitude + ',' + pos.coords.longitude;
-  }
+//   }
   return position;
+}
+
+// Pares the city response and send back the info for weather
+// State contains temperature and conditions
+function parseCity(cityResponse, state) {
+  var temperature = state.temperature;
+  var conditions = state.conditions;
+  var cityJson = JSON.parse(cityResponse);
+          
+  var address = cityJson.results[0].address_components;
+  var component = 0;
+  while(address[component].types.indexOf("locality") < 0) {
+    component++;
+  }
+
+  var city = address[component].short_name;
+
+  console.log('City is ' + city);
+  sendWeather(temperature, conditions, city);
+}
+
+// State has pos, temperature, conditions
+function parseCurrentTemp(responseText, state) {
+  var pos = state.pos;
+  
+  var json = JSON.parse(responseText);
+
+  var temperature = json.currently.temperature;
+  console.log('Temperature is ' + temperature);
+  var forecastTemp = state.temperature;
+  state.temperature = Math.round(temperature) + "/" + Math.round(forecastTemp);
+  
+  var locationUrl = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' + getWeatherLocation(pos);
+
+  xhrRequest(locationUrl, 'GET', state,
+             parseCity);
+  
+}
+
+// Parse the response temperature and get the city
+// State contains pos
+function parseForecast(responseText, state) {
+  var pos = state.pos;
+  var json = JSON.parse(responseText);
+
+  var temperature = json.daily.data[0].temperatureMax;
+  console.log('Temperature is ' + temperature);
+
+  var conditions = json.daily.data[0].icon;
+  if (conditions.indexOf('partly-cloudy') >= 0) {
+    conditions = 'Part cloud';
+  }
+  console.log('Conditions are ' + conditions);
+
+  // Get the current weather
+  var desiredDate = new Date();
+  var timeString = desiredDate.getTime()/1000|0;
+  var position = pos.coords.latitude + ',' + pos.coords.longitude;
+  var url = forecastUrl + position + ',' + timeString;
+  console.log('current url: ' + url);
+  
+  xhrRequest(url, 'GET', {pos: pos,
+                          temperature: temperature,
+                          conditions: conditions}, parseCurrentTemp);
 }
 
 // After getting the location, get the weather
@@ -64,61 +150,13 @@ function locationSuccess(pos) {
   
   console.log('Date: ' + desiredDate);
   
-  var url = 'https://api.forecast.io/forecast/dee1b0c1a1c5af5520fa94cbd4d99665/' +
+  var url = forecastUrl +
             getWeatherLocation(pos) + ',' +
             timeString;
 
   console.log('URL: ' + url);
   
-  xhrRequest(url, 'GET', 
-    function(responseText) {
-      var json = JSON.parse(responseText);
-
-      // Temperature in Kelvin requires adjustment
-      var temperature = json.daily.data[0].temperatureMax;
-      console.log('Temperature is ' + temperature);
-
-      var conditions = json.daily.data[0].icon;
-      if (conditions.indexOf('partly-cloudy') >= 0) {
-        conditions = 'Partly cloudy';
-      }
-      console.log('Conditions are ' + conditions);
-      
-      var locationUrl = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' + getWeatherLocation(pos);
-      
-      xhrRequest(locationUrl, 'GET',
-        function(cityResponse) {
-          var cityJson = JSON.parse(cityResponse);
-          
-          var address = cityJson.results[0].address_components;
-          var component = 0;
-          while(address[component].types.indexOf("locality") < 0) {
-            component++;
-          }
-          
-          var city = address[component].short_name;
-          
-          console.log('City is ' + city);
-          
-          // Assemble dictionary using our keys
-          var dictionary = {
-            'KEY_TEMPERATURE': temperature,
-            'KEY_CONDITIONS': conditions,
-            'KEY_CITY': city,
-          };
-    
-          Pebble.sendAppMessage(dictionary,
-            function(e) {
-              console.log('Weather info sent to Pebble successfully!');
-            },
-            function(e) {
-              console.log('Error sending weather info to Pebble!');
-            }
-          );
-        }
-      );
-    }
-  );
+  xhrRequest(url, 'GET', {pos: pos}, parseForecast);
 }
 
 function locationError(err) {
@@ -143,8 +181,8 @@ function getTraffic(morning) {
 //     url = "http://www.mapquestapi.com/directions/v2/route?key=affE1LXAEKtDF8KfXG7fAx0XHG7NweCe&from=37.561,-122.286&to=37.632540,-122.059575&doReverseGeocode=false";  
   }
   console.log('Traffic url: ' + url);
-  xhrRequest(url, 'GET', 
-    function(responseText) {
+  xhrRequest(url, 'GET', {}, 
+    function(responseText, state) {
 //       var json = JSON.parse(responseText);
 //       var time = json.route.realTime;
 
@@ -178,8 +216,8 @@ function getBaseballInfo(scoresQ) {
   var url = 'http://mlb.mlb.com/gdcross/components/game/mlb/year_' +
       date.getFullYear() + '/month_' + pad(date.getMonth()+1, 2) + '/day_' + pad(date.getDate(), 2) + '/master_scoreboard.json';
   console.log('Bball url: '+ url);
-  xhrRequest(url, 'GET', 
-    function(responseText) {
+  xhrRequest(url, 'GET', {}, 
+    function(responseText, state) {
       var json = JSON.parse(responseText);
       var games = json.data.games.game;
       
@@ -224,8 +262,8 @@ function getBaseballInfo(scoresQ) {
 function getNhlInfo(scoresQ) {
   console.log("NHL call");
   var url = 'http://live.nhle.com/GameData/RegularSeasonScoreboardv3.jsonp';
-  xhrRequest(url, 'GET',
-    function(res) {
+  xhrRequest(url, 'GET', {}, 
+    function(res, state) {
       var sendSuccess = function(e) {console.log('Game info sent to Pebble successfully!');};
       var sendFail = function(e) {console.log('Error sending game info to Pebble!');};
       var sentGame = false;
@@ -260,6 +298,11 @@ function getNhlInfo(scoresQ) {
               home = home.charAt(0).toUpperCase() + home.slice(1);
             }
             score_string = away + "-" + home + " " + newTime;
+          } else if (game.bs == "FINAL") {
+            if (Number(time.substring(time.indexOf("/") + 1,time.length)) == (new Date()).getDate()) {
+              score_string = game.atv[0].toUpperCase() + " " + game.ats +
+                "-" + game.hts + " " + game.htv[0].toUpperCase() + " F";
+            }
           }
           if (score_string !== '') {
             console.log('Score str: ' + score_string);
@@ -287,8 +330,8 @@ function getNbaInfo(scoresQ) {
   var date = new Date();
   var url = 'http://data.nba.com/data/1h/json/cms/noseason/scoreboard/' + date.getFullYear() + pad(date.getMonth()+1, 2) + pad(date.getDate(), 2) + '/games.json';
   console.log('NBA url: ' + url);
-  xhrRequest(url, 'GET',
-    function(res) {
+  xhrRequest(url, 'GET', {}, 
+    function(res, state) {
       var sendSuccess = function(e) {console.log('Game info sent to Pebble successfully!');};
       var sendFail = function(e) {console.log('Error sending game info to Pebble!');};
       var sentGame = false;
@@ -345,23 +388,23 @@ function getInfo() {
   console.log("Getting some info");
   var date = new Date();
   var trafficTime = false;
-  if (date.getDay() >= 1 && date.getDay() <= 5) {
-    if (date.getHours() >= 7 && date.getHours() <= 9) {
-      trafficTime = true;
-      if (date.getMinutes() % 3 === 0) {
-        getTraffic('0');
-      }
-    }
-    else if (date.getHours() >= 16 && date.getHours() <= 18) {
-      trafficTime = true;
-      if (date.getMinutes() % 3 === 0) {
-        getTraffic('1');
-      }
-    }
-  }
+//   if (date.getDay() >= 1 && date.getDay() <= 5) {
+//     if (date.getHours() >= 7 && date.getHours() <= 9) {
+//       trafficTime = true;
+//       if (date.getMinutes() % 1 === 0) {
+//         getTraffic('0');
+//       }
+//     }
+//     else if (date.getHours() >= 16 && date.getHours() <= 18) {
+//       trafficTime = true;
+//       if (date.getMinutes() % 1 === 0) {
+//         getTraffic('1');
+//       }
+//     }
+//   }
   if (!trafficTime) {
     console.log("No traffic, sports time");
-    if (date.getMinutes() % 1 === 0) {
+    if (date.getMinutes() % 2 === 0) {
       var scoresQ = [];
       scoresQ.push(returnBlank);
       scoresQ.push(getNbaInfo);
